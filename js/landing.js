6ed9fdb674ef65292a1db97ad5384114215d7f0c
Iamstefanoe <stefanoar3@gmail.com>
2024-05-22 13:11:44 +0200

    feat/cookie message

--- js/landing.js
+++ js/landing.js
@@ -1,30 +1,52 @@
-"use strict";
+'use strict';
+
+import { qS, qSA, cE } from '../utils/fn.js';
+
+const allSections = qSA('.section');
+const allButtons = qSA('button');
+const header = qS('header');
+
+//Create cookie message
+const message = cE('div');
+const messageBtn = cE('button');
+message.classList.add('cookie-message');
+message.textContent =
+  'We use cookies for improved functionality and analytics.';
+messageBtn.className = 'btn  btn--close-cookie';
+messageBtn.textContent = 'Got it!';
+message.appendChild(messageBtn);
+header.prepend(message);
+
+//Close cookie msg
+qS('.btn--close-cookie').addEventListener('click', function () {
+  message.remove();
+});
 
 // Modal window
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".btn--close-modal");
-const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const modal = qS('.modal');
+const overlay = qS('.overlay');
+const btnCloseModal = qS('.btn--close-modal');
+const btnsOpenModal = qSA('.btn--show-modal');
 
 const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", openModal);
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
